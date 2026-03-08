f378bffc31c9046aefa14724c542e3ba5ff3dce6
seekpan <lixin_qiu@163.com>
2026-03-08 14:24:44 +0800

    配置多模型

--- SpringAIAlibabaQuickStart/src/main/java/com/test/controller/DemoController.java
+++ SpringAIAlibabaQuickStart/src/main/java/com/test/controller/DemoController.java
@@ -2,22 +2,34 @@ package com.test.controller;
 
 import org.springframework.ai.chat.model.ChatModel;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 
 @RestController
 @RequestMapping("/ai")
 public class DemoController {
 
     @Autowired
-    private ChatModel chatModel;
+    @Qualifier("deepseek")
+    private ChatModel deepseekchatModel;
+
+    @Autowired
+    @Qualifier("qwen")
+    private ChatModel qwenchatModel;
 
     @GetMapping("/hello/chat")
     public String chat(@RequestParam(name = "question", defaultValue = "你是谁") String question) {
-        String result = chatModel.call(question);
+        String result = deepseekchatModel.call(question);
         return result;
+    }
+
+    @GetMapping("/hello/streamchat")
+    public Flux<String> stream(@RequestParam(name = "msg", defaultValue = "你是谁") String question) {
+        return qwenchatModel.stream(question);
     }
 
 
